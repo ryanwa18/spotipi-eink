3ac86a6563a94a887a2b2575e149e14c6c35a09e
Gabbajoe <mail@nilskeller.de>
2023-10-07 12:54:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ echo "Init git submodules"
 git submodule init
 
 echo "Add font to system:"
-sudo cp .pytho/client/static/fonts/CircularStd-Bold.otf /usr/share/fonts/opentype/CircularStd-Bold/CircularStd-Bold.otf
+sudo cp .python/client/static/fonts/CircularStd-Bold.otf /usr/share/fonts/opentype/CircularStd-Bold/CircularStd-Bold.otf
 
 echo "Installing spotipy library:"
 pip3 install spotipy --upgrade
