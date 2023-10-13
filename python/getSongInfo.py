ebf69371b534af04d3c1cfe0eba29e036ee9e645
Gabbajoe <mail@nilskeller.de>
2023-10-13 22:37:23 +0200

    workaround for random occurrent spotipy lib error: TypeError: 'NoneType' object is not subscriptable

--- python/getSongInfo.py
+++ python/getSongInfo.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import spotipy
 import spotipy.util as util
 
@@ -10,17 +11,24 @@ def getSongInfo(username: str, token_path: str) -> list:
         sp = spotipy.Spotify(auth=token)
         result = sp.current_user_playing_track()
         if result:
-            song = result["item"]["name"]
-            artist_name = ''
-            for artists_tmp in result["item"]["artists"]:
-                if artist_name:
-                    artist_name += ', '
-                artist_name += artists_tmp["name"]
-            # artist_urn = result["item"]["artists"][0]["uri"]
-            song_pic_url = result["item"]["album"]["images"][0]["url"]
-            # artist_details = sp.artist(artist_urn)
-            # artist_pic_url = artist_details['images'][0]['url']
-            return [song, song_pic_url, artist_name]  # , artist_pic_url]
+            try:
+                song = result["item"]["name"]
+                artist_name = ''
+                for artists_tmp in result["item"]["artists"]:
+                    if artist_name:
+                        artist_name += ', '
+                    artist_name += artists_tmp["name"]
+                # artist_urn = result["item"]["artists"][0]["uri"]
+                song_pic_url = result["item"]["album"]["images"][0]["url"]
+                # artist_details = sp.artist(artist_urn)
+                # artist_pic_url = artist_details['images'][0]['url']
+                return [song, song_pic_url, artist_name]  # , artist_pic_url]
+            except TypeError:
+                # https://stackoverflow.com/questions/69253296/spotipy-typeerror-nonetype-object-is-not-subscriptable-when-trying-to-acce
+                # catch this error and simply try again.
+                # try to get it in a short moment again.
+                time.sleep(0.2)
+                return getSongInfo(username, token_path)
         return []
     else:
         print(f"Error: Can't get token for {username}", file=sys.stderr)
