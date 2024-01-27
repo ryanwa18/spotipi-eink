8f137caa6a1c97a67c0599411f29ff4a9c32c0b0
Gabbajoe <mail@nilskeller.de>
2024-01-27 15:15:09 +0100

    added fix resolution back.

--- python/client/screenshot.sh
+++ python/client/screenshot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 install_path=$1
-res=$2
 #echo $install_path
-firefox --headless --screenshot /tmp/screenshot.png --window-size=$res file://$install_path/python/client/spotipi-eink.html
+firefox --headless --screenshot /tmp/screenshot.png --window-size=600,448 file://$install_path/python/client/spotipi-eink.html
 python3 $install_path/inky/examples/7color/image.py /tmp/screenshot.png
